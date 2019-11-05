0764ab946c227fa00b53e07176245a8e93f3ff8a
dev-vitalii <hrytsai.vitalii@gmail.com>
2019-11-05 17:51:41 +0200

    services settings

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -16,15 +16,14 @@ class GenusController extends Controller
     public function showAction($genusName)
     {
 
-        $notes = [
-            'Octopus asked me a riddle, outsmarted me',
-            'I counted 8 legs... as they wrapped around me',
-            'Inked!'
-        ];
+        $funFact = 'Octopuses can change the color of their body in just *three-tenths* of a second!';
+
+        $funFact = $this->get('markdown.parser')
+            ->transform($funFact);
 
         return $this->render('genus/show.html.twig', array(
             'name' => $genusName,
-            'notes' => $notes,
+            'funFact' => $funFact,
         ));
     }
 
