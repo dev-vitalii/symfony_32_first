360fea4942b9fbf7cc49f4a961162bd59983ae8a
dev-vitalii <hrytsai.vitalii@gmail.com>
2019-11-06 15:31:27 +0200

    simple database methods

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Genus;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -11,13 +12,57 @@ use Symfony\Component\Routing\Annotation\Route;
 class GenusController extends Controller
 {
     /**
-     * @Route("/genus/{genusName}")
+     * @Route("/genus/new")
+    */
+    public function newAction()
+    {
+        $genus = new Genus();
+        $genus->setName('Octopus'.rand(1,100));
+        $genus->setSpeciesCount(rand(1,10));
+        $genus->setSubFamily('Octopodinae');
+        //$genus->setFunFact( 'Fun fact))' );
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($genus);
+        $em->flush();
+
+        return new Response('<html><body>Genus created</body></html>');
+    }
+
+    /**
+     * @Route("/genus")
+     */
+    public function listAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        dump($em->getRepository('AppBundle:Genus'));
+
+        $genuses = $em->getRepository('AppBundle:Genus')
+            ->findAllPublishedOrderedBySize();
+
+
+        return $this->render('/genus/list.html.twig', [
+            'genuses' => $genuses,
+        ]);
+    }
+
+    /**
+     * @Route("/genus/{genusName}", name="genus_show")
      */
     public function showAction($genusName)
     {
 
         $funFact = 'Octopuses can change the color of their body in just *three-tenths* of a second!';
+        $em = $this->getDoctrine()->getManager();
+        $genus = $em->getRepository('AppBundle:Genus')
+            ->findOneBy(['name' => $genusName]);
 
+        if(!$genus) {
+            throw $this->createNotFoundException('Genus not found');
+        }
+
+        $funFact = $genus->getFunFact();
         $cache = $this->get('doctrine_cache.providers.my_markdown_cache');
 
         $key = md5($funFact);
@@ -30,9 +75,10 @@ class GenusController extends Controller
             $cache->save($key, $funFact);
         }
 
+        $genus->setFunFact($funFact);
+
         return $this->render('genus/show.html.twig', array(
-            'name' => $genusName,
-            'funFact' => $funFact,
+            'genus' => $genus,
         ));
     }
 
---
+++ src/AppBundle/DataFixtures/ORM/LoadFixtures.php
@@ -0,0 +1,46 @@
+<?php
+
+
+namespace AppBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Nelmio\Alice\Fixtures;
+
+class LoadFixtures implements FixtureInterface
+{
+    public function genus()
+    {
+        $genera = [
+            'Octopus',
+            'Balaena',
+            'Orcinus',
+            'Hippocampus',
+            'Asterias',
+            'Amphiprion',
+            'Carcharodon',
+            'Aurelia',
+            'Cucumaria',
+            'Balistoides',
+            'Paralithodes',
+            'Chelonia',
+            'Trichechus',
+            'Eumetopias'
+        ];
+
+        $key = array_rand( $genera );
+
+        return $genera[$key];
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $objects = Fixtures::load(
+            __DIR__.'/fixtures.yml',
+            $manager,
+            [
+                'providers' => [$this]
+            ]
+        );
+    }
+}
