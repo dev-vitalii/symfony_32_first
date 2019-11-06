a4efadfad786c68438adc07c754033974d006cf9
dev-vitalii <hrytsai.vitalii@gmail.com>
2019-11-06 10:16:03 +0200

    corrected commit

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -18,8 +18,17 @@ class GenusController extends Controller
 
         $funFact = 'Octopuses can change the color of their body in just *three-tenths* of a second!';
 
-        $funFact = $this->get('markdown.parser')
-            ->transform($funFact);
+        $cache = $this->get('doctrine_cache.providers.my_markdown_cache');
+
+        $key = md5($funFact);
+
+        if ($cache->contains($key)) {
+            $funFact = $cache->fetch($key);
+        } else {
+            $funFact = $this->get('markdown.parser')
+                ->transform($funFact);
+            $cache->save($key, $funFact);
+        }
 
         return $this->render('genus/show.html.twig', array(
             'name' => $genusName,
