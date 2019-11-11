4b3018d273571f758cfad9ace12f646a4ec228a8
dev-vitalii <hrytsai.vitalii@gmail.com>
2019-11-11 18:09:54 +0200

    login functional creating..

---
+++ src/AppBundle/Security/LoginFormAuthenticator.php
@@ -0,0 +1,76 @@
+<?php
+
+
+namespace AppBundle\Security;
+
+
+use AppBundle\AppBundle;
+use AppBundle\Entity\User;
+use AppBundle\Form\LoginForm;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Mapping\Entity;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
+
+class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
+{
+    private $formFactory;
+    private $em;
+    private $router;
+
+    public function __construct(FormFactoryInterface $formFactory, EntityManager $em, RouterInterface $router)
+    {
+        $this->formFactory = $formFactory;
+        $this->em = $em;
+        $this->router = $router;
+    }
+
+    protected function getLoginUrl()
+    {
+        return $this->router->generate('security_login');
+    }
+
+    protected function getDefaultSuccessRedirectUrl()
+    {
+        return $this->router->generate('homepage');
+    }
+
+    public function getCredentials(Request $request)
+    {
+        $isLoginSubmit = $request->attributes->get('_route') === 'security_login' && $request->isMethod('POST');
+        if (!$isLoginSubmit) {
+            // skip authentication
+            return;
+        }
+        $form = $this->formFactory->create(LoginForm::class);
+        $form->handleRequest($request);
+        $data = $form->getData();
+        $request->getSession()->set(
+            Security::LAST_USERNAME,
+            $data['_username']
+        );
+        return $data;
+    }
+
+    public function getUser($credentials, UserProviderInterface $userProvider)
+    {
+        $username = $credentials['_username'];
+        return $this->em->getRepository('AppBundle:User')
+            ->findOneBy(['email' => $username]);
+    }
+
+    public function checkCredentials($credentials, UserInterface $user)
+    {
+        $password = $credentials['_password'];
+        if ($password == 'iliketurtles') {
+            return true;
+        }
+        return false;
+    }
+}
